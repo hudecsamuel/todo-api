38fb943911afd2635e62821311c76d9f61103108
hudecsamuel <hudecsamuel@gmail.com>
2016-01-09 20:02:24 +0100

    Added PUT /todos/:id

--- server.js
+++ server.js
@@ -32,8 +32,7 @@ app.get('/todos/:id', function(req, res){
 
 //POST /todos
 app.post('/todos', function(req, res){
-  var body = req.body;
-  body = _.pick(body, 'description', 'completed');
+  var body = _.pick(req.body, 'description', 'completed');
   if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
     return res.status(400).json({"error": "Invalid data in requested fields"});
   }
@@ -46,6 +45,7 @@ app.post('/todos', function(req, res){
   res.json(body);
 });
 
+//DELETE /todos/:id
 app.delete('/todos/:id', function(req, res){
   var todoId = parseInt(req.params.id);
   var matchedTodo = _.findWhere(todos, {id: todoId});
@@ -56,6 +56,36 @@ app.delete('/todos/:id', function(req, res){
   } else {
     res.status(404).json({"error": "No todo with specified id found"});
   }
+
+});
+
+//PUT /todos/:id
+app.put('/todos/:id', function(req, res){
+  var todoId = parseInt(req.params.id);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  if(!matchedTodo){
+    return res.status(404).json({"error": "No todo with specified id found"});
+  }
+
+  var body = _.pick(req.body, 'description', 'completed');
+  var validAttributes = {};
+
+  if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+    validAttributes.completed = body.completed;
+  } else if(body.hasOwnProperty('completed')){
+    return res.status(400).json({"error": "Invalid data in 'completed' field"});
+  }
+
+  if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+    validAttributes.description = body.description.trim();
+  } else if(body.hasOwnProperty('description')){
+    return res.status(400).json({"error": "Invalid data in 'description' field"});
+  }
+
+  _.extend(matchedTodo, validAttributes);
+
+  res.json(matchedTodo);
 
 });
 
