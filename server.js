f5a3eecf43a4193c960ba32e8398e58361fa225c
hudecsamuel <hudecsamuel@gmail.com>
2016-01-09 19:15:42 +0100

    Refactored server code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3001;
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var todos = [];
 var nextTodoId = 1;
@@ -20,13 +21,7 @@ app.get('/todos', function(req, res){
 //GET /todos/:id
 app.get('/todos/:id', function(req, res){
   var todoId = parseInt(req.params.id);
-  var matchedTodo;
-
-  todos.forEach(function(todo){
-    if(todo.id === todoId){
-      matchedTodo = todo;
-    }
-  });
+  var matchedTodo = _.findWhere(todos, {id: todoId});
 
   if(matchedTodo){
     res.json(matchedTodo);
@@ -38,6 +33,12 @@ app.get('/todos/:id', function(req, res){
 //POST /todos
 app.post('/todos', function(req, res){
   var body = req.body;
+  body = _.pick(body, 'description', 'completed');
+  if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+    return res.status(400).send();
+  }
+
+  body.description = body.description.trim();
   body.id = nextTodoId++;
   todos.push(body);
 
