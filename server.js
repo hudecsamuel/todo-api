d95d081456969801b2cce63cc989df7db253f909
hudecsamuel <hudecsamuel@gmail.com>
2016-01-09 17:30:54 +0100

    Added new todo

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.port || 3001;
+var PORT = process.env.PORT || 3001;
+
+var todos = [
+  {
+  id: 1,
+  description: 'Meet mon for lunch',
+  completed: false
+},
+{
+  id: 2,
+  description: 'Meet mom for lunch',
+  completed: false
+}];
 
 app.get('/', function(req, res){
   res.send('todo api root')
