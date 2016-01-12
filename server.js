43f3a244def8e6967e4d96432bc52a18e3d38dbb
hudecsamuel <hudecsamuel@gmail.com>
2016-01-12 20:09:47 +0100

    Converted DELETE /todo/:id to sequelize

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/todos/:id', function(req, res){
   var todoId = parseInt(req.params.id);
 
   db.todo.findById(todoId).then(function(todo){
-    
+
     if(!!todo){
       //  res.status(401).json(todo);
       res.json(todo.toJSON());
@@ -75,29 +75,23 @@ app.post('/todos', function(req, res){
     res.status(e.status).json(e.message);
   });
 
-  // if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
-  //   return res.status(400).json({"error": "Invalid data in requested fields"});
-  // }
-  //
-  // body.description = body.description.trim();
-  // body.id = nextTodoId++;
-  // todos.push(body);
-  //
-  // console.log('description: ' + body.description);
-  // res.json(body);
 });
 
 //DELETE /todos/:id
 app.delete('/todos/:id', function(req, res){
   var todoId = parseInt(req.params.id);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
 
-  if(matchedTodo){
-    todos = _.without(todos, matchedTodo);
-    res.json(matchedTodo);
-  } else {
-    res.status(404).json({"error": "No todo with specified id found"});
-  }
+  db.todo.destroy({where:{
+    id: todoId
+  }}).then(function(rowsDeleted){
+    if(rowsDeleted === 0) {
+      res.status(404).json({"error": "No todo with specified id found"});
+    } else {
+      res.status(204).send();
+    }
+  }, function(err){
+    res.status(err.status).json({"error": err.message});
+  });
 
 });
 
